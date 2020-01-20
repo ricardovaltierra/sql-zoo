09c260a3ecf99f62c10dcb04def7d0b5525340e6
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-01-20 14:39:05 -0600

    0_SELECT_basics.sql update

--- 0_SELECT_basics.sql
+++ 0_SELECT_basics.sql
@@ -1,7 +1,7 @@
 -- link: https://sqlzoo.net/wiki/SELECT_basics
 
 -- 1. Modify it to show the population of Germany
-SELECT population FROM world WHERE name = 'Germany'
+SELECT population FROM world WHERE name = 'Germany';
 
 -- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway', 'Denmark');
